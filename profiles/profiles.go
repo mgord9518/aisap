8728eae80ea469eed7d92747682540578d3ffdae
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-11 08:24:38 +0000

    Fix
    Fix Minecraft permissions because it requires keyring, add pulseaudio to Librewolf

--- profiles/profiles.go
+++ profiles/profiles.go
@@ -2,34 +2,22 @@ package profiles
 
 // List of all profiles supported by aisal out of the box.
 var Profiles = map[string]AppImagePerms{
-		// TODO: add some base permissions that profiles can be built off of,
-		// because many share the same requirements (and a way to inherit them)
-//		"___BASIC_GUI___": {
-//			Level: 2,
-//			DevicePerms: []string{ "dri" },
-//			SocketPerms: []string{ "x11", "wayland" },
-//		}
 		"LibreWolf": {
 			Level: 2,
-			FilePerms:   []string{ "xdg-download:rw" },
-			DevicePerms: []string{ "dri" },
-			SocketPerms: []string{ "x11", "wayland" },
-			SharePerms:  []string{ "network" },
+			Files:   []string{ "xdg-download:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio" },
+			Share:   []string{ "network" },
 		},
 		"The Powder Toy": {
-			Level: 3,
-			DevicePerms: []string{ "dri" },
-			SocketPerms: []string{ "x11", "wayland" },
-			SharePerms:  []string{ "network" },
+			Level: 2,
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
+			Share:   []string{ "network" },
 		},
-		// Minecraft currently must be set to level 1 because it requires a
-		// valid `/etc/passwd` to launch... for some weird reason...
-		// TODO: Create fake passwd file so that sandbox levels 2 and 3 can
-		// launch apps that require it to be valid
+		// Minecraft requires access to keyring in order to launch correctly,
+		// until a fix is found Minecraft will have to be run without a sandbox
 		"Minecraft": {
-			Level: 1,
-			DevicePerms: []string{ "dri" },
-			SocketPerms: []string{ "x11", "wayland" },
-			SharePerms:  []string{ "network" },
+			Level: 0,
 		},
 	}
