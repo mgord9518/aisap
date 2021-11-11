32405a01607122127f5f1814956e213419097890
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-11 08:27:45 +0000

    Rename
    Rename struct objects to be less redundant

--- profiles/aiperms.go
+++ profiles/aiperms.go
@@ -1,9 +1,9 @@
 package profiles
 
 type AppImagePerms struct {
-	Level	      int
-	FilePerms   []string
-	DevicePerms []string
-	SocketPerms []string
-	SharePerms  []string
+	Level     int    // How much access to system files
+	Files   []string // Grant permission to access files
+	Devices []string // Access device files (eg: `/dev/dri`)
+	Sockets []string // Use sockets (eg: x11)
+	Share   []string // Share from host (eg: network)
 }
