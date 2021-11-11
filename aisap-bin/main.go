4c3a84c26e5a1889d811e695c05e58784bdd4cf7
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-11 08:08:58 +0000

    Restructure

--- aisap-bin/main.go
+++ aisap-bin/main.go
@@ -25,55 +25,100 @@
 package main
 
 import (
-    "path/filepath"
+	"fmt"
+	"os"
+	"io"
 
-    aisap    "github.com/mgord9518/aisap"
-    flag     "github.com/spf13/pflag"
-    helpers  "github.com/mgord9518/aisap/helpers"
-    profiles "github.com/mgord9518/aisap/profiles"
+	aisap	"github.com/mgord9518/aisap"
+	flag	 "github.com/spf13/pflag"
+	helpers  "github.com/mgord9518/aisap/helpers"
 )
 
 var (
-    ai   *aisap.AppImage
-    err   error
-    argv0 string
+	ai   *aisap.AppImage
+	err   error
+	argv0 string
 )
 
 // Process flags
 func main() {
-    ai, err = aisap.NewAppImage(flag.Args()[0])
-    helpers.ErrorCheck("Failed to load AppImage!", err, true)
+	ai, err = aisap.NewAppImage(flag.Args()[0])
+	helpers.ErrorCheck("Failed to load AppImage!", err, true)
 
-    // Iterate through added files and make them absolute paths
-    for i, _ := range(addFile) {
-        f, _ := filepath.Abs(addFile[i])
-        addFile[i] = f
-    }
+	// Add extra permissions as passed from flags. eg: `--add-file`
+	// Note: If *not* using XDG standard names (eg: `xdg-desktop`) you MUST
+	// Provide the full filepath when using `AddFiles`
+	ai.AddFiles(addFile)
+	ai.AddDevices(addDev)
+	ai.AddSockets(addSoc)
+	ai.AddShare(addShare)
 
-    // Add extra permissions as passed from flags. eg: `--add-file`
-    aisap.AddFilePerms(addFile)
-    aisap.AddDevicePerms(addDev)
-    aisap.AddSocketPerms(addSoc)
-    aisap.AddSharePerms(addSoc)
+	// If the `--level` flag is used, set the AppImage to that level
+	if *level > -1 && *level < 3 {
+		ai.Perms.Level = *level
+	}
 
-    var perm *profiles.AppImagePerms
+	if *permFile != "" {
+		err = ai.SetPerms(*permFile)
+		if err != nil { fmt.Println(err) }
+	}
 
-    if *permFile == "" {
-        perm, err = aisap.GetPermsFromAppImage(ai, *level)
-    } else {
-        perm, err = aisap.GetPermsFromEntry(*permFile, *level)
-    }
+	// Give basic info on the permissions the AppImage requests
+	if *listPerms && ai.Perms.Level > 0 {
+		if err != nil {
+			fmt.Println("No permissions availble for "+ai.Path)
+			os.Exit(1)
+		}
+		if len(ai.Perms.Files) > 0 {
+			fmt.Println("Files/directories:")
+			for _, v := range(ai.Perms.Files) {
+				fmt.Println(" \033[32m>\033[0m "+v)
+			}
+		}
+		if len(ai.Perms.Devices) > 0 {
+			fmt.Println("Device files:")
+			for _, v := range(ai.Perms.Devices) {
+				fmt.Println(" \033[32m>\033[0m "+v)
+			}
+		}
+		if len(ai.Perms.Sockets) > 0 {
+			fmt.Println("Sockets:")
+			for _, v := range(ai.Perms.Sockets) {
+				fmt.Println(" \033[32m>\033[0m "+v)
+			}
+		}
+		if len(ai.Perms.Share) > 0 {
+			fmt.Println("Share:")
+			for _, v := range(ai.Perms.Share) {
+				fmt.Println(" \033[32m>\033[0m "+v)
+			}
+		}
+		os.Exit(0)
+	} else if *listPerms && ai.Perms.Level == 0 {
+		fmt.Println("\033[33mApplication `"+ai.Name+"` requests to be used unsandboxed!\033[0m")
+		fmt.Println("Use the command line flag `--level [1-3]` to try to sandbox it anyway")
+		os.Exit(0)
+	}
 
-    helpers.ErrorCheck("Failed to get AppImage permissions:", err, true)
+	if err != nil{
+		fmt.Println("Failed to retrieve AppImage permissions. Attempting to launch with level 3 sandbox")
+		ai.Perms.Level = 3
 
-	// Sandbox if level is above 0, run normally if 0
-	if *level > 0 {
-		err = aisap.Wrap(ai, perm, flag.Args()[1:])
-	} else if *level == 0 {
+		err = aisap.Wrap(ai, ai.Perms, flag.Args()[1:])
+		helpers.ErrorCheck("Failed to wrap AppImage:", err, true)
+
+		aisap.UnmountAppImage(ai)
+		os.Exit(0)
+	}
+
+	// Sandbox if level is above 0
+	if ai.Perms.Level > 0 {
+		err = aisap.Wrap(ai, ai.Perms, flag.Args()[1:])
+	} else if ai.Perms.Level == 0 {
 		err = aisap.Run(ai, flag.Args()[1:])
 	}
 
-    helpers.ErrorCheck("Failed to wrap AppImage:", err, true)
+	helpers.ErrorCheck("Failed to wrap AppImage:", err, true)
 
 	// Unmount the AppImage, otherwise the user's temporary directory will
 	// get cluttered with mountpoints
