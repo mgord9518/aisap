0cefac944377ea5b7965dc57f0dd47a24456adff
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-11 08:19:38 +0000

    Restructure
    Privatize functions to get permissions from AppImage because that's now available through `ai.Perms`, rename AppImage permissions desktop flags to more logical names

--- getperms.go
+++ getperms.go
@@ -4,20 +4,18 @@ import (
 	"bytes"
 	"io/ioutil"
 	"strings"
+	"strconv"
 	"errors"
 
 	helpers  "github.com/mgord9518/aisap/helpers"
 	profiles "github.com/mgord9518/aisap/profiles"
 	ini	  "gopkg.in/ini.v1"
-	xdg	  "github.com/adrg/xdg"
 )
 
 // GetPerms attemps to read permissions from a provided desktop entry, if
 // fail, it will return an error and empty *profiles.AppImagePerms
-func GetPermsFromEntry(entryFile string, permLevel int) (*profiles.AppImagePerms, error) {
-	loadName(permLevel)
-
-	aiPerms := new(profiles.AppImagePerms)
+func getPermsFromEntry(entryFile string) (*profiles.AppImagePerms, error) {
+	aiPerms := profiles.AppImagePerms{}
 
 	if !helpers.FileExists(entryFile) {
 		return nil, errors.New("failed to find requested desktop entry! ("+entryFile+")")
@@ -32,69 +30,77 @@ func GetPermsFromEntry(entryFile string, permLevel int) (*profiles.AppImagePerms
 	entry, err := ini.Load(e)
 	if err != nil { return nil, err }
 
-	filePerms   := entry.Section("Desktop Entry").Key("X-AppImage-File-Permissions").Value()
-	devicePerms := entry.Section("Desktop Entry").Key("X-AppImage-Device-Permissions").Value()
-	socketPerms := entry.Section("Desktop Entry").Key("X-AppImage-Socket-Permissions").Value()
-	sharePerms  := entry.Section("Desktop Entry").Key("X-AppImage-Share-Permissions").Value()
+	aiPerms, err = loadPerms(aiPerms, entry)
 
-	aiPerms.Level	    = permLevel
-	aiPerms.DevicePerms = parseDevicePerms(devicePerms, permLevel)
-	aiPerms.FilePerms   = parseFilePerms(filePerms,   permLevel)
-	aiPerms.SocketPerms = parseSocketPerms(socketPerms, permLevel)
-	aiPerms.SharePerms  = parseSharePerms(sharePerms, permLevel)
-
-	if len(preFilePerms) > 0 {
-		for i, val := range(preFilePerms) {
-			nDir := val[:len(val)-3]
-			nDir = strings.Replace(nDir, xdg.Home, "/home/"+usern, 1)
-			aiPerms.FilePerms[i] = nDir
-		}
-	}
-
-	aiPerms.DevicePerms = append(preDevicePerms, aiPerms.DevicePerms...)
-	aiPerms.SocketPerms = append(preSocketPerms, aiPerms.SocketPerms...)
-
-	return aiPerms, nil
+	return &aiPerms, err
 }
 
 // Attempt to fetch permissions from the AppImage itself, fall back on internal
 // permissinos library
-func GetPermsFromAppImage(ai *AppImage, permLevel int) (*profiles.AppImagePerms, error) {
-	loadName(permLevel)
+func getPermsFromAppImage(ai *AppImage) (*profiles.AppImagePerms, error) {
+	var err error
 
-	aiPerms := new(profiles.AppImagePerms)
+	aiPerms := profiles.AppImagePerms{}
 
-	filePerms   := ai.Desktop.Section("Desktop Entry").Key("X-AppImage-File-Permissions").Value()
-	devicePerms := ai.Desktop.Section("Desktop Entry").Key("X-AppImage-Device-Permissions").Value()
-	socketPerms := ai.Desktop.Section("Desktop Entry").Key("X-AppImage-Socket-Permissions").Value()
-	sharePerms  := ai.Desktop.Section("Desktop Entry").Key("X-AppImage-Share-Permissions").Value()
+	// Use the aisap internal profile as a base if it exists
+	aiPerms = profiles.Profiles[ai.Name]
 
-	// If all keys are empty, fall back on internal library
-	if filePerms   == "" && devicePerms == "" &&
-		socketPerms == "" && sharePerms  == "" {
+	aiPerms, err = loadPerms(aiPerms, ai.Desktop)
+	if err != nil {return &aiPerms, err}
 
-		*aiPerms = profiles.Profiles[ai.Name]
-	} else {
-		aiPerms.Level	    = permLevel
-		aiPerms.DevicePerms = parseDevicePerms(devicePerms, permLevel)
-		aiPerms.FilePerms   = parseFilePerms(filePerms,   permLevel)
-		aiPerms.SocketPerms = parseSocketPerms(socketPerms, permLevel)
-		aiPerms.SharePerms  = parseSharePerms(sharePerms, permLevel)
-	}
-
-	if len(preFilePerms) > 0 {
-		for i, val := range(preFilePerms) {
-			nDir := val[:len(preFilePerms[i])-3]
-			nDir = strings.Replace(nDir, xdg.Home, "/home/"+usern, 1)
-			aiPerms.FilePerms[i] = nDir
+	// Add `:ro` if file doesn't specify
+	for i := range(aiPerms.Files) {
+		if len(strings.Split(aiPerms.Files[i], ":")) < 2 {
+			aiPerms.Files[i] = aiPerms.Files[i]+":ro"
 		}
 	}
 
-//	aiPerms.FilePerms = append(preFilePerms, aiPerms.FilePerms...)
-	aiPerms.DevicePerms = append(preDevicePerms, aiPerms.DevicePerms...)
-	aiPerms.SocketPerms = append(preSocketPerms, aiPerms.SocketPerms...)
-	aiPerms.SharePerms  = append(preSharePerms, aiPerms.SharePerms...)
+	return &aiPerms, err
+}
 
+func loadPerms(p profiles.AppImagePerms, f *ini.File) (profiles.AppImagePerms, error) {
+	// Get permissions from entry keys
+	level       := f.Section("Desktop Entry").Key("X-AppImage-Sandbox-Level").Value()
+	filePerms   := f.Section("Desktop Entry").Key("X-AppImage-Sandbox-Files").Value()
+	devicePerms := f.Section("Desktop Entry").Key("X-AppImage-Sandbox-Devices").Value()
+	socketPerms := f.Section("Desktop Entry").Key("X-AppImage-Sandbox-Sockets").Value()
+	sharePerms  := f.Section("Desktop Entry").Key("X-AppImage-Sandbox-Share").Value()
 
-	return aiPerms, nil
+	// If the AppImage desktop entry has permission flags, overwrite the
+	// profile flags
+	if len(level) > 0 {
+		p.Level, err = strconv.Atoi(level)
+
+		// Default to level 2 if requested level is invalid
+		if err != nil || p.Level > 3 || p.Level < 0 {
+			p.Level = 2
+		}
+	}
+	if len(filePerms) > 0 {
+		p.Files = helpers.DesktopSlice(filePerms)
+	}
+	if len(devicePerms) > 0 {
+		p.Devices = helpers.DesktopSlice(devicePerms)
+	}
+	if len(socketPerms) > 0 {
+		p.Sockets = helpers.DesktopSlice(socketPerms)
+	}
+	if len(sharePerms) > 0 {
+		p.Share = helpers.DesktopSlice(sharePerms)
+	}
+
+	// If all keys are still empty, throw an error
+	if len(p.Files)    == 0 && len(p.Devices) == 0 &&
+		len(p.Sockets) == 0 && len(p.Share)   == 0 {
+		err = errors.New("entry contains no permissions")
+	}
+
+	// Add `:ro` if file doesn't specify
+	for i := range(p.Files) {
+		if len(strings.Split(p.Files[i], ":")) < 2 {
+			p.Files[i] = p.Files[i]+":ro"
+		}
+	}
+
+	return p, err
 }
