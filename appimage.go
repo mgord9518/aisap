4e7f99bdc794b22cba144f3e572c24c72522b6cd
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-11 08:15:28 +0000

    Restructure
    Move several functions to the AppImage struct because it makes more sense and add a bit more compatibility with go-appimage API

--- appimage.go
+++ appimage.go
@@ -1,18 +1,22 @@
 // Drop-in replacemnt for go-appimage for sandboxing and use with shappimages
 // NOT FINISHED AND STILL LACKING BASIC FEATURES
+// THIS SHOULD BE USED FOR TESTING PURPOSES *ONLY* UNTIL IN A STABLE STATE
 
 package aisap
 
 import (
+	"bytes"
 	"path/filepath"
 	"time"
 	"io"
 	"os"
+	"strings"
 	"io/ioutil"
 
 	ini      "gopkg.in/ini.v1"
 	helpers  "github.com/mgord9518/aisap/helpers"
 	profiles "github.com/mgord9518/aisap/profiles"
+	imgconv  "github.com/mgord9518/imgconv"
 )
 
 type AppImage struct {
@@ -47,18 +51,36 @@ func NewAppImage(src string) (*AppImage, error) {
 	fp, err := filepath.Glob(ai.mountDir+"/*.desktop")
 	if err != nil { return nil, err }
 
-	// Load the first (should be only) desktop file
 	e, err := ioutil.ReadFile(fp[0])
 	entry, _ := ini.Load(e)
 
 	ai.Desktop = entry
 	ai.Name    = entry.Section("Desktop Entry").Key("Name").Value()
+	ai.Perms, _  = getPermsFromAppImage(ai)
 
     return ai, err
 }
 
-func (ai AppImage) Thumbnail() (io.ReadCloser, error) {
-	f, err := os.Open(ai.mountDir+"/.DirIcon")
+// Return a reader for the `.DirIcon` file of the AppImage, converting it to
+// PNG if it's in SVG or XPM format
+func (ai AppImage) Thumbnail() (io.Reader, error) {
+	var f io.Reader
+	f, err = os.Open(filepath.Join(ai.mountDir, ".DirIcon"))
+	if err != nil { return nil, err }
+
+	// Get the file's magic number
+	id := make([]byte, 4)
+	io.ReadAtLeast(f, id, 4)
+
+	// Convert `.DirIcon` to PNG format if it isn't already
+	// Note: the only other officially supported formats for AppImage are XPM
+	// and SVG
+	if id[0] != 0x89 || id[1] != 'P' ||
+	   id[2] != 'N'  || id[3] != 'G' {
+		// Recombine the file's magic number with the rest of the reader
+		f = io.MultiReader(bytes.NewReader(id), f)
+		f, err = imgconv.ConvertWithAspect(f, 256, "png")
+	}
 
 	return f, err
 }
@@ -73,4 +95,79 @@ func (ai AppImage) MountDir() string {
 
 func (ai AppImage) RunId() string {
 	return ai.runId
+}
+
+func (ai AppImage) AddFiles(s []string) {
+	for i := range(s) {
+		if len(strings.Split(s[i], ":")) < 2 {
+			s[i] = s[i]+":ro"
+		}
+	}
+
+	ai.Perms.Files = append(ai.Perms.Files, s...)
+}
+
+func (ai AppImage) AddDevices(s []string) {
+	ai.Perms.Devices = append(ai.Perms.Devices, s...)
+}
+
+func (ai AppImage) AddSockets(s []string) {
+	ai.Perms.Sockets = append(ai.Perms.Sockets, s...)
+}
+
+func (ai AppImage) AddShare(s []string) {
+	ai.Perms.Share = append(ai.Perms.Share, s...)
+}
+
+func (ai AppImage) SetPerms(entryFile string) error {
+	nPerms, err := getPermsFromEntry(entryFile)
+	*ai.Perms = *nPerms
+
+	return err
+}
+
+func (ai AppImage) SetRootDir(d string) {
+	rootDir = d
+}
+
+func (ai AppImage) SetDataDir(d string) {
+	dataDir = d
+}
+
+func (ai AppImage) SetTempDir(d string) {
+	tempDir = d
+}
+
+// Currently only works with type 2 AppImages, so return 2 as placeholder
+func (ai AppImage) Type() int {
+	return 2
+}
+
+// TODO: preserve file permissions
+func (ai AppImage) ExtractFile(path string, dest string, resolveSymlinks bool) error {
+	path = filepath.Join(ai.MountDir(), path)
+
+	// Remove file if it already exists
+	os.Remove(filepath.Join(dest))
+	info, err := os.Lstat(path)
+
+	// True if file is symlink and `resolveSymlinks` is false
+	if info != nil && !resolveSymlinks &&
+	info.Mode()&os.ModeSymlink == os.ModeSymlink {
+		target, _ := os.Readlink(path)
+		err = os.Symlink(target, dest)
+	} else {
+		inF, err := os.Open(path)
+		defer inF.Close()
+		if err != nil { return err }
+
+		outF, err := os.Create(dest)
+		defer outF.Close()
+		if err != nil { return err }
+
+		_, err = io.Copy(outF, inF)
+		if err != nil { return err }
+	}
+
+	return err
 }
