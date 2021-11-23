af9bdd7d48bcdef24828c63b4fb529b34199e1ad
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-23 10:47:01 +0000

    Remove case requirement and add applications

--- profiles/profiles.go
+++ profiles/profiles.go
@@ -2,22 +2,34 @@ package profiles
 
 // List of all profiles supported by aisal out of the box.
 var Profiles = map[string]AppImagePerms{
-		"LibreWolf": {
+		"librewolf": {
 			Level: 2,
 			Files:   []string{ "xdg-download:rw" },
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio" },
 			Share:   []string{ "network" },
 		},
-		"The Powder Toy": {
+		// Minecraft requires access to keyring in order to launch correctly,
+		// until a fix is found Minecraft will have to be run without a sandbox
+		"minecraft": {
+			Level: 0,
+		},
+		"supertuxkart": {
+			Level: 2,
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio" },
+			Share:   []string{ "network" },
+		},
+		"supertux 2": {
+			Level: 2,
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio" },
+			Share:   []string{ "network" },
+		},
+		"the powder toy": {
 			Level: 2,
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland" },
 			Share:   []string{ "network" },
-		},
-		// Minecraft requires access to keyring in order to launch correctly,
-		// until a fix is found Minecraft will have to be run without a sandbox
-		"Minecraft": {
-			Level: 0,
 		},
 	}
