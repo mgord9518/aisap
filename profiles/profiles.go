891fe9cc284e2e79e94dd11b5620cbf1da145416
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-25 09:27:35 +0000

    Add apps
    Add Badlion MC client, Krita and Dolphin emulator. Also added "input" device to SuperTuxKart to allow gamepads and other input devices

--- profiles/profiles.go
+++ profiles/profiles.go
@@ -1,12 +1,35 @@
 package profiles
 
 // List of all profiles supported by aisal out of the box.
+// Most of these have only been tested on my (Manjaro and Arch) systems, so they may not work correctly on yours
+// If that is the case, please report the issue and any error messages you encounter so that I can try to fix them
 var Profiles = map[string]AppImagePerms{
+		// Badlion (and others) might be able to get switched to level 2, so specify devices anyway
+		// Proprietary and unofficial AppImages should be high priority to be sandboxed to the fullest extent
+		"badlion client": {
+			Level: 1,
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio" },
+			Share:   []string{ "network" },
+		},
 		"deemix-gui": {
 			Level: 2,
 			Files:   []string{ "xdg-music:rw" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio" },
 			Share:   []string{ "network" },
+		},
+		// Dolphin could be level 2 but sound breaks, need to find a solution to that
+		"dolphin emulator": 
+			Level: 1,
+			Files:   []string{ "xdg-download:ro", "~/Games:ro", "~/Roms:ro" },
+			Devices: []string{ "dri", "input" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio" },
+		},
+		"krita": {
+			Level: 2,
+			Files:   []string{ "xdg-pictures:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
 		},
 		"librewolf": {
 			Level: 2,
@@ -22,7 +45,7 @@ var Profiles = map[string]AppImagePerms{
 		},
 		"supertuxkart": {
 			Level: 2,
-			Devices: []string{ "dri" },
+			Devices: []string{ "dri", "input" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio" },
 			Share:   []string{ "network" },
 		},
