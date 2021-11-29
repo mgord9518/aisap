d6c5007dc5145e84acfc540d9b4dd0e3a3b7f1a4
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-29 12:36:29 +0000

    Changes
    Added a flag to disable color, added a version flag (counting in hexadecimal), renamed variables to be consistent with the flag names, separated the examples to keep the help menu a little less cluttered and added a handle to properly clean up if [CTRL]+C is used.

--- aisap-bin/flags.go
+++ aisap-bin/flags.go
@@ -9,11 +9,13 @@ import (
 
 type arrayFlags []string
 
-const (
+var (
 	g = "\033[32m" // Green
 	y = "\033[33m" // Yellow
 	r = "\033[31m" // Red
 	z = "\033[0m"  // Reset
+
+	ver = "000001"
 )
 
 var (
@@ -23,53 +25,61 @@ var (
     listPerms = flag.BoolP("list-perms", "l", false, "")
 
     // Long-only flags
-    permFile = flag.String("profile", "", "")
-    level    = flag.Int("level",        -1, "")
+    color   =   flag.Bool("color",   true,  "")
+    example =   flag.Bool("example", false, "")
+    version =   flag.Bool("version", false, "")
+    profile = flag.String("profile", "",    "")
+    level   =    flag.Int("level",   -1,    "")
 
 	// Flags that can be called multiple times
-	addFile  arrayFlags
-	addDev   arrayFlags
-	addSoc   arrayFlags
-	addShare arrayFlags
+	file   arrayFlags
+	device arrayFlags
+	socket arrayFlags
+	share  arrayFlags
 )
 
 // Initialization of global variables and help menu
 func init() {
     var present bool
+	handleCtrlC()
 
-    flag.Var(&addFile,  "file",   "")
-    flag.Var(&addDev,   "device", "")
-    flag.Var(&addSoc,   "socket", "")
-    flag.Var(&addShare, "share",  "")
+    flag.Var(&file,   "file",   "")
+    flag.Var(&device, "device", "")
+    flag.Var(&socket, "socket", "")
+    flag.Var(&share,  "share",  "")
 
 	// Prefer AppImage-provided variable `ARGV0` if present
     if argv0, present = os.LookupEnv("ARGV0"); !present {
         argv0 = os.Args[0]
     }
 
-    flag.Usage = func() {
+    flag.Parse()
 
+	// Remove color if `color=false`
+	if !*color {
+		g = ""
+		y = ""
+		r = ""
+	}
+
+    flag.Usage = func() {
         fmt.Printf("Usage: %s%s%s [OPTIONS] [APPIMAGE]\n\n", g, argv0, z)
         fmt.Printf("Easily sandbox AppImages in BubbleWrap\n")
         fmt.Printf("With no PERMFILE, read permissions directly from AppImage\n")
         fmt.Printf("Sandbox level of 0 only changes data directory, not actually sandboxed!\n\n")
         fmt.Printf("%sNormal options:\n", y)
-        fmt.Printf("%s  -h, --help    %sDisplay this help menu\n", g, z)
-        fmt.Printf("%s  -v, --verbose %sBe more verbose (NEI)\n\n", g, z)
+        fmt.Printf("%s  -h, --help     %sDisplay this help menu\n", g, z)
+        fmt.Printf("%s  -v, --verbose  %sBe more verbose (NEI)\n\n", g, z)
         fmt.Printf("%sLong-only options:\n", y)
-        fmt.Printf("%s  --file    %sAdd file to sandbox\n", g, z)
-        fmt.Printf("%s  --socket  %sAllow access to additional sockets\n", g, z)
-		fmt.Printf("%s  --share   %sAdd share to sandbox (eg: network)\n", g, z)
-        fmt.Printf("%s  --device  %sAllow access to additional /dev files\n", g ,z)
-        fmt.Printf("%s  --level   %sChange the base security level of the sandbox (min: 0, max: 3)\n", g, z)
-        fmt.Printf("%s  --profile %sLook for permissions in this entry instead of the AppImage\n\n", g, z)
-        fmt.Printf("%sExamples:%s\n", y, z)
-        fmt.Printf("  %s%s --profile%s=./f.desktop -- ./f.app\n", g, argv0, z)
-        fmt.Printf("    Sandbox `f.app` using permissions from `f.desktop`\n\n")
-        fmt.Printf("  %s%s ./f.app --level%s=2\n", g, argv0, z)
-        fmt.Printf("    Tighten `f.app` sandbox to level 2 (default: 1)\n\n")
-        fmt.Printf("  %s%s --file%s=./f.txt %s--file%s ./other.bin ./f.app\n", g, argv0, z, g, z)
-        fmt.Printf("    Allow sandbox to access files `f.txt` and `other.bin`\n\n")
+		fmt.Printf("%s  --color    %sWhether color should be shown (default: true)\n", g, z)
+		fmt.Printf("%s  --example  %sShow usage examples\n", g, z)
+        fmt.Printf("%s  --file     %sAdd file to sandbox\n", g, z)
+        fmt.Printf("%s  --socket   %sAllow access to additional sockets\n", g, z)
+		fmt.Printf("%s  --share    %sAdd share to sandbox (eg: network)\n", g, z)
+        fmt.Printf("%s  --device   %sAllow access to additional /dev files\n", g ,z)
+        fmt.Printf("%s  --level    %sChange the base security level of the sandbox (min: 0, max: 3)\n", g, z)
+        fmt.Printf("%s  --profile  %sLook for permissions in this entry instead of the AppImage\n", g, z)
+        fmt.Printf("%s  --version  %sPrint the version and exit\n\n", g, z)
         fmt.Printf("%sWARNING:%s No sandbox is impossible to escape! This is to *aid* security, not\n", r, z)
         fmt.Printf("guarentee safety when downloading sketchy stuff online. Don't be stupid!\n\n")
         fmt.Printf("Plus, this is ALPHA software! Very little testing has been done;\n")
@@ -77,7 +87,21 @@ func init() {
         os.Exit(0)
     }
 
-    flag.Parse()
+	if *version {
+        fmt.Println(ver)
+		os.Exit(0)
+	}
+
+	if *example {
+        fmt.Printf("%sExamples:%s\n", y, z)
+        fmt.Printf("  %s%s --profile%s=./f.desktop -- ./f.app\n", g, argv0, z)
+        fmt.Printf("    Sandbox `f.app` using permissions from `f.desktop`\n\n")
+        fmt.Printf("  %s%s ./f.app --level%s=2\n", g, argv0, z)
+        fmt.Printf("    Tighten `f.app` sandbox to level 2 (default: 1)\n\n")
+        fmt.Printf("  %s%s --file%s=./f.txt %s--file%s ./other.bin ./f.app\n", g, argv0, z, g, z)
+        fmt.Printf("    Allow sandbox to access files `f.txt` and `other.bin`\n")
+		os.Exit(0)
+	}
 
     if *help || len(os.Args) < 2 {
         flag.Usage()
