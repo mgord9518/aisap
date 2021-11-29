225f8984add31745e4871cc2f659b64bfc516430
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-29 03:39:46 +0000

    Delete aiperms.go

--- profiles/aiperms.go
+++
@@ -1,9 +0,0 @@
-package profiles
-
-type AppImagePerms struct {
-	Level     int    // How much access to system files
-	Files   []string // Grant permission to access files
-	Devices []string // Access device files (eg: `/dev/dri`)
-	Sockets []string // Use sockets (eg: x11)
-	Share   []string // Share from host (eg: network)
-}
