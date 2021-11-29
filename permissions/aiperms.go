defcd6bbde65c14143f581728543116b2072f6ab
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-11-29 00:23:26 +0000

    Create aiperms.go

---
+++ permissions/aiperms.go
@@ -0,0 +1,9 @@
+package permissions
+
+type AppImagePerms struct {
+	Level     int    // How much access to system files
+	Files   []string // Grant permission to access files
+	Devices []string // Access device files (eg: `/dev/dri`)
+	Sockets []string // Use sockets (eg: x11)
+	Share   []string // Share from host (eg: network)
+}
