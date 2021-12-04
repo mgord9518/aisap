01a817d0c4ecf3c0188d39b3f442e1c98c29c8a6
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-12-04 23:34:57 +0000

    Simplify and properly rename desktop entry section

--- getperms.go
+++ getperms.go
@@ -62,57 +62,48 @@ func getPermsFromAppImage(ai *AppImage) (*permissions.AppImagePerms, error) {
 	return &aiPerms, err
 }
 
+// Load permissions from INI
 func loadPerms(p permissions.AppImagePerms, f *ini.File) (permissions.AppImagePerms, error) {
-	err = nil
+	var err error
 
-	// Get permissions from entry keys
-	level       := f.Section("Required Permissions").Key("Level").Value()
-	filePerms   := f.Section("Required Permissions").Key("Files").Value()
-	devicePerms := f.Section("Required Permissions").Key("Devices").Value()
-	socketPerms := f.Section("Required Permissions").Key("Sockets").Value()
-	sharePerms  := f.Section("Required Permissions").Key("Share").Value()
+	// Get permissions from keys
+	level       := f.Section("X-AppImage-Required-Permissions").Key("Level").Value()
+	filePerms   := f.Section("X-AppImage-Required-Permissions").Key("Files").Value()
+	devicePerms := f.Section("X-AppImage-Required-Permissions").Key("Devices").Value()
+	socketPerms := f.Section("X-AppImage-Required-Permissions").Key("Sockets").Value()
 
-	// If the AppImage desktop entry has permission flags, overwrite the
-	// profile flags
 	if level != "" {
 		l, err := strconv.Atoi(level)
 
 		if err != nil || l < 0 || l > 3 {
 			p.Level = -1
-			err = errors.New("invalid permissions level (must be 0-3)")
+			return p, errors.New("invalid permissions level (must be 0-3)")
 		} else {
 			p.Level = l
 		}
 	} else {
 		p.Level = -1
-		err = errors.New("profile does not have required flag `Level` under section [Required Permissions]")
-	}
-	if len(filePerms) > 0 {
-		p.Files = helpers.DesktopSlice(filePerms)
-	}
-	if len(devicePerms) > 0 {
-		p.Devices = helpers.DesktopSlice(devicePerms)
-	}
-	if len(socketPerms) > 0 {
-		p.Sockets = helpers.DesktopSlice(socketPerms)
-	}
-	if len(sharePerms) > 0 {
-		p.Share = helpers.DesktopSlice(sharePerms)
+		return p, errors.New("profile does not have required flag `Level` under section [X-AppImage-Required-Permissions]")
 	}
 
-	// If all keys are still empty, throw an error
-	if len(p.Files)    == 0 && len(p.Devices) == 0 &&
-		len(p.Sockets) == 0 && len(p.Share)   == 0 {
-		err = errors.New("entry contains no permissions")
-	}
+	p.Files = helpers.SplitKey(filePerms)
+	p.Devices = helpers.SplitKey(devicePerms)
+	p.Sockets = helpers.SplitKey(socketPerms)
 
-	// Add `:ro` if file doesn't specify
+	// Assume readonly if unspecified
 	for i := range(p.Files) {
 		ex := p.Files[i][len(p.Files[i])-3:]
 
 		if len(strings.Split(p.Files[i], ":")) < 2 ||
 		ex != ":ro" && ex != ":rw" {
 			p.Files[i] = p.Files[i]+":ro"
+		}
+	}
+
+	// Convert devices to shorthand if not already
+	for i, val := range(p.Devices) {
+		if len(val) > 5 && val[0:5] == "/dev/" {
+			p.Devices[i] = strings.Replace(val, "/dev/", "", 1)
 		}
 	}
 
