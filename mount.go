04f51e5301c3290ebde75ef07ad50ee1e7b4f3a0
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-12-04 23:58:06 +0000

    Check for existence before trying to unmount

--- mount.go
+++ mount.go
@@ -35,6 +35,12 @@ func Mount(src string, dest string, offset int) error {
 
 // Unmounts an AppImage
 func Unmount(ai *AppImage) error {
+	if (ai == nil) {
+		return errors.New("AppImage is nil")
+	} else if ai.Path == "" {
+		return errors.New("AppImage contains no path")
+	}
+
 	err = unmountDir(ai.MountDir())
 	if err != nil { return err }
 
