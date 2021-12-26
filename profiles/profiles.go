b6dead431ad14f41dff1b3eeb3b086061f8b9d17
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2021-12-26 13:46:49 +0000

    Add apps
    Add apps, fix Dolphin emulator to be level 2 and fix a comment that I forgot to finish writing

--- profiles/profiles.go
+++ profiles/profiles.go
@@ -8,6 +8,10 @@ import (
 // Most of these have only been tested on my (Manjaro and Arch) systems, so they may not work correctly on yours
 // If that is the case, please report the issue and any error messages you encounter so that I can try to fix them
 var Profiles = map[string]permissions.AppImagePerms{
+		// Any apps that require superuser can't be sandboxed in this way
+		"balenaetcher": {
+			Level: 0,
+		},
 		// Badlion (and others) might be able to get switched to level 2, so specify devices anyway
 		// Proprietary and unofficial AppImages should be high priority to be sandboxed to the fullest extent
 		"badlion client": {
@@ -27,11 +31,22 @@ var Profiles = map[string]permissions.AppImagePerms{
 			Files:   []string{ "xdg-music:rw" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio", "network" },
 		},
-		// Dolphin could be level 2 but sound breaks, need to find a solution to that
 		"dolphin emulator": {
-			Level: 1,
+			Level: 2,
 			Files:   []string{ "xdg-download:ro", "~/Games:ro", "~/Roms:ro" },
 			Devices: []string{ "dri", "input" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio" },
+		},
+		"dust3d": {
+			Level: 2,
+			Devices: []string{ "dri" },
+			Files:   []string{ "xdg-templates:rw", "xdg-documents:rw" },
+			Sockets: []string{ "x11", "wayland" },
+		},
+		"endless sky": {
+			Level: 2,
+			Devices: []string{ "dri" },
+			Files:   []string{ "xdg-templates:rw", "xdg-documents:rw" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio" },
 		},
 		"firefox": {
@@ -52,6 +67,25 @@ var Profiles = map[string]permissions.AppImagePerms{
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio", "network" },
 		},
+		"fontforge": {
+			Level: 2,
+			Files:   []string{ "xdg-documents:rw", "~/.fonts:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
+		},
+		// Also requires `/etc/passwd`
+		"freecad conda": {
+			Level: 1,
+			Files:   []string{ "xdg-documents:rw", "xdg-templates:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
+		},
+		"geometrize": {
+			Level: 2,
+			Files:   []string{ "xdg-pictures:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
+		},
 		"gnu image manipulation program": {
 			Level: 1,
 			Files:   []string{ "xdg-pictures:rw" },
@@ -64,7 +98,7 @@ var Profiles = map[string]permissions.AppImagePerms{
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland" },
 		},
-		// Network for 
+		// Network for plugins and syncing
 		"joplin": {
 			Level: 2,
 			Files:   []string{ "xdg-documents:rw" },
@@ -77,11 +111,22 @@ var Profiles = map[string]permissions.AppImagePerms{
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland" },
 		},
+		"leocad": {
+			Level: 2,
+			Files:   []string{ "xdg-documents:rw", "xdg-templates:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
+		},
 		"librewolf": {
 			Level: 2,
 			Files:   []string{ "xdg-download:rw" },
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio", "network" },
+		},
+		"linedancer": {
+			Level: 2,
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
 		},
 		"liteide": {
 			Level: 2,
@@ -101,6 +146,21 @@ var Profiles = map[string]permissions.AppImagePerms{
 		// until a fix is found Minecraft will have to be run without a sandbox
 		"minecraft": {
 			Level: 0,
+		},
+		// Minetest demonstrates that not all GUI apps need level 2 or lower
+		// fully self-contained apps that don't use system fonts, etc. can be
+		// run in level 3
+		"minetest": {
+			Level: 3,
+			Files:   []string{ "xdg-download:rw" },
+			Devices: []string{ "dri", "input" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio", "network" },
+		},
+		"mypaint": {
+			Level: 2,
+			Files:   []string{ "xdg-pictures:rw", "xdg-templates:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
 		},
 		// Write access given to because save files are stored in the same
 		// directory as the rom
@@ -154,11 +214,23 @@ var Profiles = map[string]permissions.AppImagePerms{
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland", "pulseaudio", "network" },
 		},
+		// Audio for notification sounds
+		"sengi": {
+			Level: 1,
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland", "pulseaudio", "network" },
+		},
 		// Link to device not tested
 		"signal": {
 			Level: 2,
 			Devices: []string{ "dri" },
 			Sockets: []string{ "x11", "wayland", "network" },
+		},
+		"smallbasic": {
+			Level: 2,
+			Files:   []string{ "xdg-documents:rw" },
+			Devices: []string{ "dri" },
+			Sockets: []string{ "x11", "wayland" },
 		},
 		// I think it's an interesting idea to have a confined terminal
 		// enviornment although it could also greatly hinder its usefullness
