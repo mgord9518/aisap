5c4c001fe961b7c55686647e72bc12143ee4e80e
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-01-13 09:45:44 +0000

    Move file spook checker to its own package
    The point of this is to have a good list of files and directories that may be potential escape vectors if write access is granted, along with a list of other files that may lead to leakage of important information if read access is given.
    
    Basically, check if accessing certain files or directories is spooky behavior (outside of the apps that are actually supposed to use those files), warning the user if it is.

---
+++ spooky/isspooky.go
@@ -0,0 +1,59 @@
+package spooky
+
+import (
+	"strings"
+)
+
+// Check if a file or directory is spooky (sandbox escape vector) so that the
+// user can be warned that their sandbox is insecure
+// TODO: expand this list! There are a lot of files that can be used to escape
+// the sandbox, while it's impossible to cover all bases, we should try to get
+// as close as possible
+func IsSpooky(str string) bool {
+	// An app with access to these specific paths may be able to use them for escape
+	spookyFiles := []string{
+		"~",
+		"/",
+		"/tmp",
+		"/tmp/aisap",
+		"/home",
+		"~/Apps",
+		"~/Applications",
+		"~/AppImages",
+		"~/.profile",
+		"~/.bashrc",
+		"~/.zshrc",
+	}
+
+	// Requesting these, or any file under them at all could be a potential
+	// escape route or leak personal information
+	spookyDirs := []string{
+		"~/.ssh",
+		"~/.firefox",
+		"~/.mozilla",
+		"~/.local",
+		"~/.config",
+	}
+
+	// Split the string into its actual directory and whether it's read only or
+	// read write
+	slice := strings.Split(str, ":")
+	s1 := strings.Join(slice[:len(slice)-1], ":")
+	s2 := ":"+strings.Split(str, ":")[len(slice)-1]
+
+	for _, val := range(spookyFiles) {
+		if s1 == val && s2 == ":rw" {
+			return true
+		}
+	}
+
+	for _, val := range(spookyDirs) {
+		if len(s1) >= len(val) {
+			if s1[:len(val)] == val {
+				return true
+			}
+		}
+	}
+
+	return false
+}
