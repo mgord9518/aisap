2c2a30aee01ff50c998fc68b64037f0629c21683
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-01-15 12:59:47 +0000

    Add flags for removing permissions

--- aisap-bin/flags.go
+++ aisap-bin/flags.go
@@ -16,7 +16,7 @@ var (
 	c = "\033[0;36m" // Cyan
 	z = "\033[0;0m"  // Reset
 
-	ver = "0.1.29"
+	ver = "0.2.0"
 )
 
 var (
@@ -33,9 +33,12 @@ var (
 	level   =    flag.Int("level",   -1,    "")
 
 	// Flags that can be called multiple times
-	file   arrayFlags
-	device arrayFlags
-	socket arrayFlags
+	file     arrayFlags
+	device   arrayFlags
+	socket   arrayFlags
+	rmfile   arrayFlags
+	rmdevice arrayFlags
+	rmsocket arrayFlags
 )
 
 // Initialization of global variables and help menu
@@ -46,6 +49,9 @@ func init() {
 	flag.Var(&file,   "file",   "")
 	flag.Var(&device, "device", "")
 	flag.Var(&socket, "socket", "")
+	flag.Var(&rmfile,   "rmfile",   "")
+	flag.Var(&rmdevice, "rmdevice", "")
+	flag.Var(&rmsocket, "rmsocket", "")
 
 	// Prefer AppImage-provided variable `ARGV0` if present
 	if argv0, present = os.LookupEnv("ARGV0"); !present {
@@ -53,24 +59,29 @@ func init() {
 	}
 
 	flag.Usage = func() {
-		fmt.Printf("Usage: %s%s%s [OPTIONS] [APPIMAGE]\n\n", g, argv0, z)
-		fmt.Printf("Easily sandbox AppImages in BubbleWrap\n")
-		fmt.Printf("With no PERMFILE, read permissions directly from AppImage\n")
-		fmt.Printf("Sandbox level of 0 only changes data directory, not actually sandboxed!\n\n")
-		fmt.Printf("%sNormal options:\n", y)
-		fmt.Printf("%s  -h, --help     %sDisplay this help menu\n", g, z)
-		fmt.Printf("%s  -v, --verbose  %sBe more verbose (NEI)\n\n", g, z)
+		fmt.Printf("usage: %s%s%s [OPTIONS] [APPIMAGE]\n\n", g, argv0, z)
+		fmt.Printf("wrapper binary around aisap to easily sandbox AppImages in BubbleWrap\n")
+		fmt.Printf("with no PERMFILE, read permissions directly from AppImage or internal\n")
+		fmt.Printf("permissions library\n")
+		fmt.Printf("sandbox level of 0 only changes data directory, not actually sandboxed!\n\n")
+		fmt.Printf("%snormal options:\n", y)
+		fmt.Printf("%s  -h, --help     %sdisplay this help menu\n", g, z)
+//		fmt.Printf("%s  -v, --verbose  %sbe more verbose (NEI)\n\n", g, z)
 		fmt.Printf("%sLong-only options:\n", y)
-		fmt.Printf("%s  --color    %sWhether color should be shown (default: true)\n", g, z)
-		fmt.Printf("%s  --example  %sShow usage examples\n", g, z)
-		fmt.Printf("%s  --file     %sAdd file to sandbox\n", g, z)
-		fmt.Printf("%s  --socket   %sAllow access to additional sockets\n", g, z)
-		fmt.Printf("%s  --device   %sAllow access to additional /dev files\n", g ,z)
-		fmt.Printf("%s  --level    %sChange the base security level of the sandbox (min: 0, max: 3)\n", g, z)
-		fmt.Printf("%s  --profile  %sLook for permissions in this entry instead of the AppImage\n", g, z)
-		fmt.Printf("%s  --version  %sPrint the version and exit\n\n", g, z)
-		fmt.Printf("%sWARNING:%s No sandbox is impossible to escape! This is to *aid* security, not\n", r, z)
+		fmt.Printf("%s  --color    %swhether color should be shown (default: true)\n", g, z)
+		fmt.Printf("%s  --example  %sshow usage examples\n", g, z)
+		fmt.Printf("%s  --file     %sadd file to sandbox\n", g, z)
+		fmt.Printf("%s  --socket   %sallow access to additional sockets\n", g, z)
+		fmt.Printf("%s  --device   %sallow access to additional /dev files\n", g ,z)
+		fmt.Printf("%s  --rmfile   %sremove file from sandbox\n", g, z)
+		fmt.Printf("%s  --rmsocket %sremove sandbox access to socket\n", g, z)
+		fmt.Printf("%s  --rmdevice %sremove access to device file\n", g ,z)
+		fmt.Printf("%s  --level    %schange the base security level of the sandbox (min: 0, max: 3)\n", g, z)
+		fmt.Printf("%s  --profile  %slook for permissions in this entry instead of the AppImage\n", g, z)
+		fmt.Printf("%s  --version  %sprint the version and exit\n\n", g, z)
+		fmt.Printf("%sWARNING:%s no sandbox is impossible to escape! This is to *aid* security, not\n", r, z)
 		fmt.Printf("guarentee safety when downloading sketchy stuff online. Don't be stupid!\n\n")
+		fmt.Printf("for more information, visit <://github.com/mgord9518/aisap>\n\n")
 		fmt.Printf("Plus, this is ALPHA software! Very little testing has been done;\n")
 		fmt.Printf("%sUSE AT YOUR OWN RISK!%s\n", r, z)
 		os.Exit(0)
@@ -92,13 +103,13 @@ func init() {
 	}
 
 	if *example {
-		fmt.Printf("%sExamples:%s\n", y, z)
+		fmt.Printf("%sexamples:%s\n", y, z)
 		fmt.Printf("  %s%s --profile%s=./f.desktop -- ./f.app\n", g, argv0, z)
-		fmt.Printf("    Sandbox `f.app` using permissions from `f.desktop`\n\n")
+		fmt.Printf("    sandbox `f.app` using permissions from `f.desktop`\n\n")
 		fmt.Printf("  %s%s ./f.app --level%s=2\n", g, argv0, z)
-		fmt.Printf("    Tighten `f.app` sandbox to level 2 (default: 1)\n\n")
+		fmt.Printf("    tighten `f.app` sandbox to level 2 (default: 1)\n\n")
 		fmt.Printf("  %s%s --file%s=./f.txt %s--file%s ./other.bin ./f.app\n", g, argv0, z, g, z)
-		fmt.Printf("    Allow sandbox to access files `f.txt` and `other.bin`\n")
+		fmt.Printf("    allow sandbox to access files `f.txt` and `other.bin`\n")
 		os.Exit(0)
 	}
 
