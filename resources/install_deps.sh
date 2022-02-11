3d1e709ec3908b0131ada3582b54e243ae21cebe
Mathew Gordon <mgord9518@gmail.com>
2022-02-11 21:19:45 +0900

    Fix rsvg name

--- resources/install_deps.sh
+++ resources/install_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install appstream squashfs-tools optipng squashfuse rsvg-convert
+sudo apt-get install appstream squashfs-tools optipng squashfuse librsvg2-bin
