c5e145f4eb746a3808cd388394b0058b8d88614e
Mathew Gordon <mgord9518@gmail.com>
2022-02-20 23:47:45 +0900

    Display TMPDIR for debugging

--- resources/test.sh
+++ resources/test.sh
@@ -2,9 +2,11 @@
 # Make sure everything runs
 # Will eventually make it more in-depth
 
+echo "$TMPDIR"
 ./*.shImg --help
 [ $? -ne 0 ] && exit 1
 
+echo "$TMPDIR"
 ./*.AppImage --help
 [ $? -ne 0 ] && exit 1
 
