684bd2191a18cde4a48d2fa3223663812025d297
Mathew Gordon <mgord9518@ubuntu-22-04.fedora>
2023-07-26 04:51:04 -0600

    Add libfuse3 to deps

--- resources/install_deps.sh
+++ resources/install_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install appstream squashfs-tools squashfuse librsvg2-bin
+sudo apt-get install appstream squashfs-tools squashfuse librsvg2-bin libfuse3-dev
